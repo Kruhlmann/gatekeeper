50133fe1ddc473fa6c655b089b0f0c90eae4824d
Andreas Kruhlmann <kruhlmann@protonmail.com>
2019-10-30 06:20:30 +0000

    Role assignment complete

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ import { handle_exception, log } from "./io";
 import { LoggingLevel } from "./typings/types";
 import * as config from "../config.json";
 import * as captcha_generator from "../captchas";
+import { createHash } from "crypto";
 
 process.on("uncaughtException", handle_exception);
 process.on("unhandledRejection", handle_exception);
@@ -25,12 +26,14 @@ function send_captcha(user: discord.GuildMember) {
     const message = new discord.RichEmbed()
         .setTitle("Fight Club Captcha")
         .setDescription(captcha.text)
-        .addField("Answer", captcha.answer)
-        .addField("Seed", captcha.seed)
         .setThumbnail("https://img.rankedboost.com/wp-content/uploads/2019/05/WoW-Classic-Warrior-Guide-150x150.png")
+
+    const hasher = createHash("sha256");
+    hasher.update(captcha.seed + captcha.answer)
 
     user.send(captcha_preface)
     user.send(message);
+    user.send(`\`${captcha.seed}:${hasher.digest("hex")}\``);
 }
 
 /**
@@ -74,8 +77,52 @@ function role_routine(guild: discord.Guild, read_role: discord.Role): void {
         setInterval(() => role_routine(guild, read_role), 5000);
     });
 
-    //discord_client.on("message", (message: discord.Message) => {
-    //});
+    discord_client.on("message", (message: discord.Message) => {
+        if (message.channel.type !== "dm") {
+            return;
+        }
+        const channel = message.channel as discord.DMChannel;
+        const messages_promise = channel.fetchMessages({})
+        const guild = discord_client.guilds.get(config.guild_id);
+        const write_role = guild.roles.get(config.role_ids.write);
+        const user = guild.members.get(message.author.id);
+        const has_write_role = !!user.roles.find((role) => {
+            return role.id === config.role_ids.write;
+        });
+
+        if (has_write_role || message.author.id === discord_client.user.id) {
+            return;
+        }
+
+        messages_promise.then((messages: Map<any, discord.Message>) => {
+            const queue: string[] = [];
+            for (const [m_id, m] of messages.entries()) {
+                if (m.author.id !== discord_client.user.id || !m.content.match(/\w+:\w{64,64}/g)) {
+                    continue;
+                }
+                const seed = m.content.split(":")[0].replace("`", "");
+                const answer = m.content.split(":")[1].replace("`", "");
+
+                const hasher = createHash("sha256");
+                hasher.update(seed + message.content);
+                const user_answer = hasher.digest("hex");
+
+                if (user_answer === answer) {
+                    user.addRole(write_role);
+                    const usr_str = `<${user.user.username}:${user.id}>`;
+                    const role_str = `<${write_role.name}:${write_role.id}>`;
+                    log(`Added read role ${role_str} to user ${usr_str}`);
+                    queue.push(`\`${message.content}\` is correct. You've been given write permissions to the channels.`)
+                } else {
+                    queue.push(`\`${message.content}\` is not correct.`)
+                }
+                break;
+            }
+            if (queue.length > 0) {
+                message.channel.send(queue[0])
+            }
+        });
+    });
 
     discord_client.on("guildMemberAdd", (user: discord.GuildMember) => {
         send_captcha(user);
