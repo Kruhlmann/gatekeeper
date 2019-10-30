80068323c2dfb99bd423939aedc1bb407f836254
Andreas Kruhlmann <kruhlmann@protonmail.com>
2019-10-30 02:15:51 +0000

    Role detection

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,20 @@ import * as config from "../config.json";
 process.on("uncaughtException", handle_exception);
 process.on("unhandledRejection", handle_exception);
 
+function remove_chromosomes(collection: Map<string, any>) {
+    return Object.fromEntries(collection.entries())
+}
+
+function purge_routine(guild: discord.Guild, read_role: discord.Role) {
+    guild.members.forEach((user) => {
+        const has_role = !!user.roles.find((role) => role.id === read_role.id);
+        if (!has_role) {
+            user.addRole(read_role);
+            log(`Added read role to user <${user.user.username}:${user.id}>`);
+        }
+    });
+}
+
 (async () => {
     // Init discord virtual client.
     const discord_client = new discord.Client();
@@ -20,12 +34,31 @@ process.on("unhandledRejection", handle_exception);
         : config.discord_bot_token.development;
 
     log("Awaiting response from discord", LoggingLevel.DEV);
+
     discord_client.on("ready", () => {
         log(`Started gatekeeper in ${config.deployment_mode} mode`);
+        const guild = discord_client.guilds.get(config.guild_id)
+        const read_role = guild.roles.get(config.role_id)
+        // TODO
+        purge_routine(guild, read_role)
+        //setInterval(() => purge_routine(discord_client), 1000)
     });
 
     discord_client.on("message", (message: discord.Message) => {
-        message.author.send("xd");
+        const channel = message.channel;
+
+        if (channel.type == "text") {
+            const guild = (channel as discord.TextChannel).guild;
+
+            if (guild.id !== config.guild_id) {
+                return;
+            }
+
+            const roles = guild.roles;
+            console.log(JSON.stringify(Object.keys(discord_client.guilds), null, 4))
+
+        }
+
     });
 
     discord_client.on("guildMemberAdd", (user: discord.GuildMember) => {
@@ -33,11 +66,8 @@ process.on("unhandledRejection", handle_exception);
         if (guild.id !== config.guild_id) {
             return;
         }
-
-        console.log(Object.keys(guild.roles)
-
+        console.log(Object.keys(guild.roles));
     });
-
 
     // Authenticate.
     discord_client.login(dicord_token).catch(handle_exception);
