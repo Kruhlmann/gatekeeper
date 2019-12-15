080a15982b39b752e774bc20a551b2c9a75d8525
Andreas Kruhlmann <kruhlmann@protonmail.com>
2019-12-15 16:53:14 +0100

    Started typescript based unit tests

---
+++ tests/captcha.spec.ts
@@ -0,0 +1,18 @@
+import { expect } from "chai";
+import "mocha";
+
+describe("CAPTCHAs", () => {
+    describe("Hit chance", () => {
+        describe("Orc", () => {
+            it("should return true", () => {
+                expect(1).to.equal(1)
+            });
+        });
+        describe("Orc", () => {
+            it("should return true", () => {
+                expect(1).to.equal(1)
+            });
+        });
+    });
+});
+
